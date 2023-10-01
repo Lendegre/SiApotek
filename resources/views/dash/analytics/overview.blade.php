679638f1a4739b8853bb0be9dc0744ab84a86103
nepile <nevillejro.laia@gmail.com>
2023-10-02 04:43:09 +0700

    style: update overview layout

--- resources/views/dash/analytics/overview.blade.php
+++ resources/views/dash/analytics/overview.blade.php
@@ -22,7 +22,9 @@
                             <h1 class="mt-1 mb-3">2.382</h1>
                         </div>
                     </div>
+                </div>
 
+                <div class="col-sm-6">
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
@@ -41,41 +43,6 @@
                     </div>
                 </div>
 
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Earnings</h5>
-                                </div>
-
-                                <div class="col-auto">
-                                    <div class="stat text-primary">
-                                        <i class="align-middle" data-feather="dollar-sign"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <h1 class="mt-1 mb-3">$21.300</h1>
-                        </div>
-                    </div>
-
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Orders</h5>
-                                </div>
-
-                                <div class="col-auto">
-                                    <div class="stat text-primary">
-                                        <i class="align-middle" data-feather="shopping-cart"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <h1 class="mt-1 mb-3">64</h1>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
